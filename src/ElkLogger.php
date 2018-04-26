ad0b39cfed388ddf4910b795b7ba0bf243e8b22a
Reasno <guxi99@gmail.com>
2018-04-26 11:14:57 +0800

    first commit

---
+++ src/ElkLogger.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Reasno\ElkLogger;
+
+use Monolog\Logger;
+use Monolog\Handler\RedisHandler;
+use Monolog\Formatter\LogstashFormatter;
+use Predis\Client;
+
+class ElkLogger
+{
+    /**
+     * Create a custom Monolog instance.
+     *
+     * @param  array  $config
+     * @return \Monolog\Logger
+     */
+    public function __invoke(array $config)
+    {
+        // Init a RedisHandler with a LogstashFormatter.
+        // The parameters may differ depending on your configuration of Redis.
+        // Important: The parameter 'logs' must be equal to the key you defined
+        // in your logstash configuration.
+        $client = new Client([
+            'scheme' => 'tcp',
+            'host'   => $config['host'],
+            'port'   => $config['port'],
+        ]);
+
+        $client->auth($config['password']);
+
+        $redisHandler = new RedisHandler($client, 'logstash');
+        $formatter = new LogstashFormatter('sso');
+        $redisHandler->setFormatter($formatter);
+        return new Logger('sso', [$redisHandler]);
+    }
+}
